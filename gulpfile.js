d1288ed18a5c13d6d7fb898ea0190fc9021486ac
Luke <chenkuanlu@yeah.net>
2020-09-02 17:03:29 +0800

    项目打包

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 
-var{ src , dest , series , parallel , watch } = require('gulp');
+var { src , dest , series , parallel , watch } = require('gulp');
 var clean = require('gulp-clean');
 var fileInclude = require('gulp-file-include');
 var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var requirejsOptimize = require('gulp-requirejs-optimize');
 
 function cleanTask(){
-    return src('/dist' , {allowEmpty : true})
-            .pipe( clean() )
+    return src('./dist' , {allowEmpty : true})
+            .pipe( clean() );
 }
 
 function fileIncludeTask(){
@@ -20,40 +24,40 @@ function fileIncludeTask(){
 }
 
 function webserverTask(){
-    return src ('./dist')
+    return src('./dist')      //是view文件夹下作为localhost根目录
             .pipe( webserver({
                 host : 'localhost',
                 port : 4000,
                 open : './view/index.html',
                 livereload : true
-            }))
+            }));
 }
 
 function sassTask(){
     return src('./src/css/*.scss')
             .pipe(sass())
-            .pipe(dest('./dist/css'))
+            .pipe(dest('./dist/css'));
 }
 
-function staticTask(){
+function staticTask(){   //把src中的静态资源同步到dist下
     return src('./src/static/**')
-            .pipe(dest('./dist/static'))
+            .pipe(dest('./dist/static'));
 }
 
 function libTask(){
     return src('./src/lib/**')
-            .pipe(dest('./dist/lib'))
+            .pipe( dest('./dist/lib') );
 }
 function apiTask(){
     return src('./src/api/**')
-            .pipe(dest('./dist/api'))
+            .pipe( dest('./dist/api') );
 }
 function jsTask(){
     return src('./src/js/**')
-            .pipe(dest('./dist/js'))
+            .pipe( dest('./dist/js') );
 }
 
-function watchTask(){
+function watchTask(){   //监听文件变化，同步到dist文件下
     watch('./src/view/**' , fileIncludeTask);
     watch('./src/css/**' , sassTask);
     watch('./src/static/**' , staticTask);
@@ -62,9 +66,31 @@ function watchTask(){
     watch('./src/js/**' , jsTask);
 }
 
+function buildCSSTask(){
+    return src('./src/css/*.scss')
+            .pipe(sass())
+            .pipe(cssmin())
+            .pipe(dest('./dist/css'));
+}
 
+function buildJSTask(){
+    return src('./src/js/*.js')
+            .pipe(requirejsOptimize({
+                optimize:"none",
+                paths:{                        
+                    "jquery":"empty:"  //不会把jquery模块合并进去
+                }
+            }))
+            .pipe(babel({
+                presets: ['es2015']
+             }))
+            .pipe(uglify())
+            .pipe( dest('./dist/js') );
+}
 
 module.exports = {
-    dev : series(cleanTask  , parallel(fileIncludeTask , sassTask , staticTask , libTask , apiTask , jsTask) , parallel(webserverTask , watchTask) ),
-    build : series(cleanTask)
+    // 开发环境下的命令
+    dev : series( cleanTask , parallel(fileIncludeTask , sassTask , staticTask , libTask , apiTask , jsTask) , parallel(webserverTask , watchTask) ),    
+    // 生产环境下的命令
+    build : series( cleanTask , parallel(fileIncludeTask , buildCSSTask , buildJSTask , staticTask , libTask , apiTask) )
 };
